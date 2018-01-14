49ff23689545585e9c5109843062089829aa7c43
Kasia RN <rudyknowak@gmail.com>
2018-01-14 20:59:17 +0100

    zadanie-java-31 punkt 1

---
+++ src/Car.java
@@ -0,0 +1,26 @@
+public class Car {
+
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car (String m, String c, int t , double p){
+        brand = m;
+        model=c;
+        year=t;
+        capacity=p;
+
+
+   //String model;
+   //String color;
+   //String type;
+   //int price;
+
+   //Car (String m, String c, String t, int p) {
+       //model=m;
+      // color=c;
+      // type=t;
+      // price=p;
+   }
+}
