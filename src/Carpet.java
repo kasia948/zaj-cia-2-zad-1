4a3c9919637292eb645dfdbdfa43e19a7d53258b
Kasia RN <rudyknowak@gmail.com>
2018-01-14 20:03:48 +0100

    zad 1.2

--- src/Carpet.java
+++ src/Carpet.java
@@ -2,4 +2,10 @@ public class Carpet {
     String size;
     String colour;
     String shape;
+
+    Carpet(String s, String c, String p ) {
+        size=s;
+        colour=c;
+        shape=p;
+    }
 }
--- src/Home.java
+++ src/Home.java
@@ -1,14 +1,14 @@
 public class Home {
     public static void main(String[] args) {
-        Carpet carpet1=new Carpet();
-        carpet1.size="big";
-        carpet1.colour="red";
-        carpet1.shape="square";
+        Carpet carpet1=new Carpet("big", "red","square");
+        //carpet1.size="big";
+       // carpet1.colour="red";
+        //carpet1.shape="square";
 
-        Carpet carpet2=new Carpet();
-        carpet2.size="small";
-        carpet2.colour="black";
-        carpet2.shape="round";
+        Carpet carpet2=new Carpet( "small","balck","round" );
+        //carpet2.size="small";
+       // carpet2.colour="black";
+       // carpet2.shape="round";
 
         System.out.println("I have two carpets in my house, one is "+carpet1.size+", "+carpet1.colour+" and "+carpet1.shape+
                 ". Another one is "+carpet2.size+", "+carpet2.colour+" and "+carpet2.shape+".");
